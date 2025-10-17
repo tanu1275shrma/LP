351f0c8b6f4f9f351834df6438bf81c74e460554
Tania Sharma <tanushrma1022@gmail.com>
2025-10-17 17:24:58 +0530

    app.jsx chnaged

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 import LandingPage from "./components/LandingPage.jsx";
-import Webinar from "./components/Webinar.jsx";
 import ThankYouPage from "./components/ThankyouPage.jsx";
 import Secrets from "./components/Secrets.jsx";
 import DidYouKnow from "./components/DidYouKnow.jsx";
@@ -18,27 +17,49 @@ import WhoIsThisFor from "./components/WhoIsThisFor.jsx";
 import ReviewSection from "./components/ReviewSection.jsx";
 
 function App() {
+  useEffect(() => {
+    // ✅ Load Facebook Pixel script
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(
+      window,
+      document,
+      "script",
+      "https://connect.facebook.net/en_US/fbevents.js"
+    );
+
+    window.fbq("init", "2259159114243836");
+    window.fbq("track", "PageView");
+  }, []);
+
   return (
     <>
-      {/* ✅ Facebook Meta Pixel Code */}
       <Helmet>
-        {`<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2259159114243836');
-fbq('track', 'PageView');
-</script>`}
-        <noscript>{`<img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=2259159114243836&ev=PageView&noscript=1"
-/>`}</noscript>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2259159114243836&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </Helmet>
-      {/* ✅ End Meta Pixel Code */}
 
       <Routes>
         <Route
